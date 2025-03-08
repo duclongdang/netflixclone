2ed6cb7dbb7b1e8adc8abc65051ab73d44ad490a
duclongdang <duclongdang838@gmail.com>
2025-03-08 15:59:14 +0700

    protect-route-middleware

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,18 @@ import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 import movieRoute from "./routes/movie.route.js";
 import tvRoute from "./routes/tv.route.js";
+import cookieParser from "cookie-parser";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); //req.body (parse body)
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/movie", movieRoute);
-app.use("/api/v1/tv", tvRoute);
+app.use("/api/v1/movie", protectRoute, movieRoute);
+app.use("/api/v1/tv", protectRoute, tvRoute);
 
 app.listen(PORT, () => {
   console.log("Server started on http://localhost:" + PORT);
